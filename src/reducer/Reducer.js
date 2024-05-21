6b938434707d4d8c5025679f439529ec963b67ce
elifred2022 <elifed21@hotmail.com>
2024-05-20 22:24:39 -0300

    limpie el reduer

--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,13 +1,6 @@
 export const initialState = {
   comidas: [],
-
-  bebidas: [],
   montoComidaGral: [],
-  montoPorcentaje: [],
-  indicesComidas: [],
-  montoBebidaCu: [],
-  resultado: [],
-  resultadoEfectivo: [],
 };
 
 export default function Reducer(state, action) {
@@ -27,15 +20,6 @@ export default function Reducer(state, action) {
       };
     }
 
-    case "EDITAR_CONSUMO": {
-      return {
-        ...state,
-        consumoStore: state.consumoStore.map((consumo) =>
-          consumo.id === action.payload.id ? action.payload : consumo
-        ),
-      };
-    }
-
     case "ELIMINAR_COMIDA": {
       return {
         ...state,
@@ -45,100 +29,11 @@ export default function Reducer(state, action) {
       };
     }
 
-    case "AGREGAR_PORCENTAJE":
-      return {
-        ...state,
-        montoPorcentaje: [...state.montoPorcentaje, action.payload],
-      };
-
-    case "EDITAR_PORCENTAJE": {
-      return {
-        ...state,
-        montoPorcentaje: state.montoPorcentaje.map((porcent) =>
-          porcent.id === action.payload.id ? action.payload : porcent
-        ),
-      };
-    }
-
-    case "ELIMINAR_PORCENTAJE": {
-      return {
-        ...state,
-        montoPorcentaje: state.montoPorcentaje.filter(
-          (porcent) => porcent.descuento !== action.payload.descuento
-        ),
-      };
-    }
-
-    case "AGREGAR_BEBIDA":
-      return {
-        ...state,
-        bebidas: [...state.bebidas, action.payload],
-      };
-
-    case "EDITAR_BEBIDA": {
-      return {
-        ...state,
-        bebidas: state.bebidas.map((bebida) =>
-          bebida.id === action.payload.id ? action.payload : bebida
-        ),
-      };
-    }
-
-    case "ELIMINAR_BEBIDA": {
-      return {
-        ...state,
-        bebidas: state.bebidas.filter(
-          (bebida) => bebida.bebida !== action.payload.bebida
-        ),
-      };
-    }
-
-    case "AGREGAR_INDICE":
-      return {
-        ...state,
-        indicesComidas: [...state.indicesComidas, action.payload],
-      };
-
-    case "AGREGAR_RESULTADO":
-      return {
-        ...state,
-        resultado: [...state.resultado, action.payload],
-      };
-
-    case "EDITAR_RESULTADO": {
-      return {
-        ...state,
-        resultado: state.resultado.map((result) =>
-          result.id === action.payload.id ? action.payload : result
-        ),
-      };
-    }
-
-    case "AGREGAR_RESULTADOEFECTIVO":
-      return {
-        ...state,
-        resultadoEfectivo: [...state.resultadoEfectivo, action.payload],
-      };
-
     case "AGREGAR_MONTOTOTALCOMIDA":
       return {
         ...state,
         montoComidaGral: [...state.montoComidaGral, action.payload],
       };
-
-    case "AGREGAR_BEBIDACU":
-      return {
-        ...state,
-        montoBebidaCu: [...state.montoBebidaCu, action.payload],
-      };
-
-    case "RESET":
-      return initialState;
-    // other action types...
-
-    case "RESET_RESULTADOS":
-      return { ...state, resultado: [], resultadoEfectivo: [] };
-    // other action types...
 
     default: {
       throw Error("Unknown action: " + action.type);
